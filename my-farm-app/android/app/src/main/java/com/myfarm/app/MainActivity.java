c22ed346a925a3116ee316762ff331bc157708ec
Blessing Jackson <jaydenj772@gmail.com>
2024-10-01 19:19:51 +0200

    delete frontend and farm app

--- my-farm-app/android/app/src/main/java/com/myfarm/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.myfarm.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- my-farm-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.myfarm.app',
-  appName: 'my-farm-app',
-  webDir: 'dist'
-};
-
-export default config;
