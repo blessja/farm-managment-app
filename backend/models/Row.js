edf001c4d70fc84ecab0e4e4950f9bbbf6136936
Blessing Jackson <jaydenj772@gmail.com>
2024-10-11 20:52:55 +0200

    Update server and controllers

--- backend/models/Row.js
+++ backend/models/Row.js
@@ -6,7 +6,10 @@ const rowSchema = new mongoose.Schema({
   worker_id: { type: String, default: "" },
   time_spent: { type: Number, default: 0 }, // time in minutes
   stock_count: { type: Number, default: 0 },
+  remaining_stock_count: Number, // number of stocks remaining
+  bunches: { type: Number, default: 0 },
   start_time: { type: Date, default: null },
+  stocks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Stock" }], // array of stock ids
 });
 
 const Row = mongoose.model("Row", rowSchema);
