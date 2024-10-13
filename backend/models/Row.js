e7a7a8686f903ecaa9a695931d5b1c74fef708da
Blessing Jackson <jaydenj772@gmail.com>
2024-10-13 13:13:18 +0200

    worker document

--- backend/models/Row.js
+++ backend/models/Row.js
@@ -9,13 +9,7 @@ const rowSchema = new mongoose.Schema({
   remaining_stock_count: Number, // number of stocks remaining
   bunches: { type: Number, default: 0 },
   start_time: { type: Date, default: null },
-  daily_stock_entries: [
-    {
-      date: { type: Date },
-      stock_count: { type: Number },
-      time_spent: { type: Number },
-    },
-  ], // store stock count entries for the row
+  // store stock count entries for the row
 });
 
 const Row = mongoose.model("Row", rowSchema);
