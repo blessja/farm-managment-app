0b59b2bea63b2d618685a43b94df7e4a99883eec
Blessing Jackson <jaydenj772@gmail.com>
2024-10-24 20:25:34 +0200

    reverse the clock lunch break

--- backend/controllers/workerClock.js
+++ backend/controllers/workerClock.js
@@ -55,14 +55,12 @@ exports.addClockOut = async (req, res) => {
   const { workerID, workerName } = req.body;
 
   try {
-    // Find the worker by workerID and workerName
     const worker = await WorkerClock.findOne({ workerID, workerName });
 
     if (!worker) {
       return res.status(404).json({ message: "Worker not found" });
     }
 
-    // Find the current active clock-in session without a clock-out time
     const currentSession = worker.clockIns.find(
       (session) => !session.clockOutTime
     );
@@ -73,43 +71,32 @@ exports.addClockOut = async (req, res) => {
         .json({ message: `Worker ${workerName} is not clocked in.` });
     }
 
-    // Set the clock-out time to the current time
+    // Set clock-out time and calculate duration
     currentSession.clockOutTime = new Date();
-
-    // Calculate the total duration in hours
-    let duration =
+    const duration =
       (new Date(currentSession.clockOutTime) -
         new Date(currentSession.clockInTime)) /
       1000 /
       60 /
       60; // Convert milliseconds to hours
-
-    // Deduct 1 hour if the duration is greater than 4 hours (lunch break)
-    if (duration > 4) {
-      duration -= 1;
-    }
-
-    // Update the duration field in the session
     currentSession.duration = duration;
 
-    // Get the day of the week from the clock-in session
+    // Get the day of the week
     const clockInDay = currentSession.day;
 
     // Update the hours worked on that specific day
     worker.workedHoursPerDay[clockInDay] += duration;
 
-    // Add the duration to the total worked hours
+    // Add to total worked hours
     worker.totalWorkedHours += duration;
 
-    // Save the worker record
     await worker.save();
-
     res.json({
       message: `Worker ${
         worker.workerName
       } clocked out successfully. Worked ${duration.toFixed(
         2
-      )} hours (including 1-hour lunch deduction) on ${clockInDay}.`,
+      )} hours on ${clockInDay}.`,
     });
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
